363e38b04ebd0a7618fd58bf3da0c2111a51f2b2
Kaleb Molewe <88307946+KalebMolewe23@users.noreply.github.com>
2024-02-08 14:55:06 +0700

    update midtrans

--- config/session.php
+++ config/session.php
@@ -32,6 +32,7 @@ return [
     */
 
     'lifetime' => env('SESSION_LIFETIME', 120),
+    // 'lifetime' => null,
 
     'expire_on_close' => false,
 
@@ -197,18 +198,5 @@ return [
     */
 
     'same_site' => 'lax',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Partitioned Cookies
-    |--------------------------------------------------------------------------
-    |
-    | Setting this value to true will tie the cookie to the top-level site for
-    | a cross-site context. Partitioned cookies are accepted by the browser
-    | when flagged "secure" and the Same-Site attribute is set to "none".
-    |
-    */
-
-    'partitioned' => false,
 
 ];
