5c2635a3a9ce24aa07e88ec6dedf260ef910cd03
Kaleb Molewe <88307946+KalebMolewe23@users.noreply.github.com>
2024-03-11 20:57:09 +0700

    100% fix fitur admin

--- resources/views/admin/ticket/v_ticket.blade.php
+++ resources/views/admin/ticket/v_ticket.blade.php
@@ -74,7 +74,171 @@
                 <div class="menu-inner-shadow"></div>
                 <ul class="menu-inner py-1">
             
-                @include('agen.layout.menu_admin')
+                <!-- Dashboard -->
+            <li class="menu-item">
+              <a href="/admin/dashboard" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Dashboard</div>
+              </a>
+            </li>
+            <!-- tiket -->
+            <li class="menu-header small text-uppercase"><span class="menu-header-text">Ticket</span></li>
+            <li class="menu-item">
+              <a href="javascript:void(0)" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bxs-plane"></i>
+                <div data-i18n="Extended UI">Data Ticket Umroh</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="/admin/add_bursa_ticket" class="menu-link">
+                    <div data-i18n="Perfect Scrollbar">Add Bursa Ticket</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="/admin/ticket_group" class="menu-link">
+                    <div data-i18n="Text Divider">Data Tiket Group</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="/admin/bursa_ticket" class="menu-link">
+                    <div data-i18n="Text Divider">Data Bursa Ticket</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="menu-item active">
+              <a href="/admin/provider" class="menu-link">
+                <i class="menu-icon tf-icons bx bxs-user-voice"></i>
+                <div data-i18n="Basic">Provider</div>
+              </a>
+            </li>
+            <!-- umroh -->
+            <li class="menu-header small text-uppercase"><span class="menu-header-text">Umroh</span></li>
+            <li class="menu-item">
+              <a href="/admin/data_packet_umroh" class="menu-link">
+                <i class="menu-icon tf-icons bx bxs-package"></i>
+                <div data-i18n="Basic">Data Packet Umroh</div>
+              </a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:void(0)" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bxs-building"></i>
+                <div data-i18n="Extended UI">Data Hotel</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="/admin/jeddah_hotel" class="menu-link">
+                    <div data-i18n="Perfect Scrollbar">Jeddah</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="/admin/madinah_hotel" class="menu-link">
+                    <div data-i18n="Text Divider">Madinah</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="menu-item">
+              <a href="/admin/category_umroh" class="menu-link">
+                <i class="menu-icon tf-icons bx bxs-category"></i>
+                <div data-i18n="Basic">Category</div>
+              </a>
+            </li>
+            <!-- customer -->
+            <li class="menu-header small text-uppercase"><span class="menu-header-text">Customer</span></li>
+            <li class="menu-item">
+              <a href="/admin/data_customer" class="menu-link">
+                <i class="menu-icon tf-icons bx bxs-user-rectangle"></i>
+                <div data-i18n="Basic">Data Member</div>
+              </a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:void(0)" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bxs-briefcase-alt"></i>
+                <div data-i18n="Extended UI">Data Travel</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="/admin/travel_information" class="menu-link">
+                    <div data-i18n="Perfect Scrollbar">Information Travel</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="/admin/information_branch" class="menu-link">
+                    <div data-i18n="Text Divider">Information Branch</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="/admin/information_worsiphers" class="menu-link">
+                    <div data-i18n="Text Divider">Data Jamaah</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <!-- CMS -->
+            <li class="menu-header small text-uppercase"><span class="menu-header-text">CMS</span></li>
+            <li class="menu-item">
+              <a href="/admin/bg_color/1" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-palette"></i>
+                <div data-i18n="Basic">Background Color</div>
+              </a>
+            </li>
+            <li class="menu-item">
+              <a href="/admin/information/1" class="menu-link">
+                <i class="menu-icon tf-icons bx bxs-info-circle"></i>
+                <div data-i18n="Basic">Information</div>
+              </a>
+            </li>
+            <li class="menu-item">
+              <a href="/admin/sosmed/" class="menu-link">
+                <i class="menu-icon tf-icons bx bxl-stripe"></i>
+                <div data-i18n="Basic">Social Media</div>
+              </a>
+            </li>
+            <li class="menu-item">
+              <a href="/admin/blog/" class="menu-link">
+                <i class="menu-icon tf-icons bx bxl-blogger"></i>
+                <div data-i18n="Basic">Blog</div>
+              </a>
+            </li>
+            <li class="menu-item">
+              <a href="/admin/covereds/" class="menu-link">
+                <i class="menu-icon tf-icons bx bxs-book-content"></i>
+                <div data-i18n="Basic">Data Peliput</div>
+              </a>
+            </li>
+
+            <!-- Layouts -->
+            <li class="menu-item">
+
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="layouts-without-menu.html" class="menu-link">
+                    <div data-i18n="Without menu">Without menu</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="layouts-without-navbar.html" class="menu-link">
+                    <div data-i18n="Without navbar">Without navbar</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="layouts-container.html" class="menu-link">
+                    <div data-i18n="Container">Container</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="layouts-fluid.html" class="menu-link">
+                    <div data-i18n="Fluid">Fluid</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="layouts-blank.html" class="menu-link">
+                    <div data-i18n="Blank">Blank</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
 
                 </ul>
             </aside>
